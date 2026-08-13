77c65fe506d0284cd15cf0a16ed7cc451d90cb07
samruddhi-19 <samruddhibarahate@gmail.com>
2026-08-14 02:31:38 +0530

    updated API configuration1

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.ProgressConfig = {
   API_KEY: "93b1fabac6fe3f9a688c9b4cc836f97d",
   APP_NAME: "Progress Tracker",
-  API_BASE: "https://us-central1-YOUR_PROJECT_ID.cloudfunctions.net/progressApi",
+  
 };
